23fc849932efc597e890009c8c2faa107851f623
lokendra pandey <lokendrapandey5278@gmail.com>
2024-09-15 22:07:48 +0530

    bugs fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import About from './Components/About';
 import Contact from './Components/Contact';
-import Projects from './Components/Projects';
+import Projects from './Components/Projects.jsx';
 import 'aos/dist/aos.css';
 
 
